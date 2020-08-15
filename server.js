051d68a163e8af0ef475210da5cc915326085f7b
andre_viallon <viallon.andre@gmail.com>
2020-08-15 16:25:04 +0200

    clean up code

--- server.js
+++ server.js
@@ -10,25 +10,20 @@ dotenv.config({ path: './config/config.env' });
 connectDB();
 
 const todos = require('./routes/todos');
+
 const app = express();
 
 app.use(express.json());
+
+app.use('/api/v1/todos', todos);
 
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
-app.use('/api/v1/todos', todos);
-
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
-}
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build')));
 }
 
 const PORT = process.env.PORT || 5000;
